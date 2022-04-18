275e90a51199bd8a69c55e68aff27c79ad24cf23
tiahelou10 <tia.elhelou@outlook.com>
2022-04-19 02:02:30 +0300

    Edited the Sign up.php file

--- Back-end/Sign up.php
+++ Back-end/Sign up.php
@@ -12,16 +12,28 @@ $points = 0;
  
 $query = $mysqli->prepare("SELECT user_id FROM users WHERE user_name = $name and user_phone_number = $phone;");
 $query->execute();
-$result = $query->get_result();
+$account_result = $query->get_result();
 
-if(empty($result)){
+$query = $mysqli->prepare("SELECT user_id FROM users WHERE user_username = $username;");
+$query->execute();
+$username_result = $query->get_result();
 
-	$query = $mysqli->prepare("INSERT INTO users (user_name, user_username, user_password, user_gender, user_date_of_birth, user_phone_number, user_points) VALUES (?, ?, ?, ? , ?, ?)");
-	$query->bind_param("sssssii", $name , $username , $password , $gender, $birthday , $phone , $points);
-	$query->execute();
+if(empty($account_result)){
+	if(empty($username_result)){
+		$query = $mysqli->prepare("INSERT INTO users (user_name, user_username, user_password, user_gender, user_date_of_birth, user_phone_number, user_points) VALUES (?, ?, ?, ? , ?, ?)");
+		$query->bind_param("sssssii", $name , $username , $password , $gender, $birthday , $phone , $points);
+		$query->execute();
+
+		echo "Account created successfully";
+	}
+	else{
+		echo "Username already exist!";
+	}
+
+	
 }
 else{
-	echo "You already have an account!"
+	echo "You already have an account!";
 }
 
 ?>
