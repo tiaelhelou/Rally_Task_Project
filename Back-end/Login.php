eac4cb4aae74578a9d5557b4569a7447aa8362f6
tiahelou10 <tia.elhelou@outlook.com>
2022-04-20 01:19:52 +0300

    Created and implemneted the login function (Back-end)

---
+++ Back-end/Login.php
@@ -0,0 +1,21 @@
+<?php
+
+include('ConnecttoDb\my_db.php'); 
+
+$username = $_POST['user_username'];
+$password = $_POST['user_password'];
+
+$query = $mysqli->prepare("SELECT user_id FROM users WHERE user_username = $username and user_password = $password;");
+$query->execute();
+$account_result = $query->get_result();
+
+if(empty($account_result)){
+	
+	echo "Incorrect username or/and password"
+	
+}
+else{
+	echo "Loged in successfully";
+}
+
+?>
