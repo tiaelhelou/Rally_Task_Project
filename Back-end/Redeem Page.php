02929f6b5aa0f28919e0d32dc720bbb36a5c5312
tiahelou10 <tia.elhelou@outlook.com>
2022-04-21 01:20:03 +0300

    Edited the Edit Profile to get all the gifts

--- Back-end/Redeem Page.php
+++ Back-end/Redeem Page.php
@@ -6,11 +6,25 @@ include('Login')
 
 $query = $mysqli->prepare("SELECT user_points FROM users WHERE user_username = $username and user_password = $password;");
 $query->execute();
-$account_result = $query->get_result();
-$row = mysqli_fetch_row($account_result);
+$point_result = $query->get_result();
+$row = mysqli_fetch_row($point_result);
 $points = $row[0];
 
 echo "User's points: " + $points;
+
+
+$query = $mysqli->prepare("SELECT gift_name, gift_points FROM gifts ;");
+$query->execute();
+$gift_result = $query->get_result();
+
+$gifts = [];
+
+while($gifts = $gift_result->fetch_assoc()){
+    $gifts[] = $gifts;
+}
+
+$json_buy_sell = json_encode($buy_sell);
+echo $json_buy_sell;
 
 
 ?>
