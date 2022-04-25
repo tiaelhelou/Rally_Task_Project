e60b0758b9694f5310846cd8bc0100b5aa12f6d4
tiahelou10 <tia.elhelou@outlook.com>
2022-04-25 13:09:16 +0300

    Created the edit profile page

--- Front-end/rally-task/src/app/app-routing.module.ts
+++ Front-end/rally-task/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'editprofile',
+    loadChildren: () => import('./pages/editprofile/editprofile.module').then( m => m.EditprofilePageModule)
+  },
 ];
 
 @NgModule({
