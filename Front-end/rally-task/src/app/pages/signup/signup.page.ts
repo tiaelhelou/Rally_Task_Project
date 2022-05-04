009805d8f92f0831ed6af66c2257660d93b3a541
tiahelou10 <tia.elhelou@outlook.com>
2022-05-04 20:44:00 +0300

    Created sign up service

--- Front-end/rally-task/src/app/pages/signup/signup.page.ts
+++ Front-end/rally-task/src/app/pages/signup/signup.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { SignupService, User } from '../../services/signups/signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  user: User;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: SignupService) { }
 
   ngOnInit() {
   }
 
-  
-  signup(){
+  onSubmit(form: NgForm) {
+    this.user = form.value;
+    this.service.addUser(this.user).subscribe(response => {
+      console.log(response);
+    })
+  }
+
+  signup() {
     this.router.navigate(['/login']);
   }
 }
--- Front-end/rally-task/src/app/services/signups/signup.service.ts
+++ Front-end/rally-task/src/app/services/signups/signup.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface User {
+  user_name : string,
+  user_username : string,
+  user_password : string,
+  user_gender : string,
+  user_date_of_birth : string,
+  user_phone_number : string
+  
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  private url= "http://localhost/RallyTask/";
+
+  constructor(private http: HttpClient) { }
+
+  addUser(user:User){
+    return this.http.post(this.url+ "Sign up.php", JSON.stringify(user));
+  }
+    
 }
