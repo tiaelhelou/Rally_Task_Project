86ffd2f958b704129f6590932e290143b8cd0672
tiahelou10 <tia.elhelou@outlook.com>
2022-05-05 22:44:44 +0300

    created editprofile service

--- Front-end/rally-task/src/app/pages/editprofile/editprofile.page.ts
+++ Front-end/rally-task/src/app/pages/editprofile/editprofile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { EditprofileService, User } from '../../services/editprofiles/editprofile.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -8,7 +10,10 @@ import { Router } from '@angular/router';
 })
 export class EditprofilePage implements OnInit {
 
-  constructor(private router: Router) { }
+  user: User;
+  status: any;
+
+  constructor(private router: Router, private service: EditprofileService, public toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -21,4 +26,20 @@ export class EditprofilePage implements OnInit {
     this.router.navigate(['/login']);
   }
 
+  onSubmit(form: NgForm) {
+    this.user = form.value;
+    this.service.addUser(this.user).subscribe (response => {this.status = response;} )
+
+    if (this.status.status == true){
+      this.presentToast();
+      this.router.navigate(['/userprofile']);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Updated Succefully',
+      duration: 2000
+    });
+    toast.present();
+  }
 }
--- Front-end/rally-task/src/app/services/editprofiles/editprofile.service.ts
+++ Front-end/rally-task/src/app/services/editprofiles/editprofile.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+export interface User {
+  user_name : string,
+  user_username : string,
+  user_password : string,
+  user_gender : string,
+  user_date_of_birth : string,
+  user_phone_number : number,
+  user_interest_category: string
+  
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditprofileService {
 
-  constructor() { }
+  private url= "http://localhost/RallyTask/";
+
+  constructor(private http: HttpClient) { }
+
+  addUser(user:User){
+    return this.http.post(this.url+ "Edit Profile.php", JSON.stringify(user));
+  }
 }
